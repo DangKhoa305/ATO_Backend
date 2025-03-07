e97da82a8a2b316c8811514b3e899d8640002b4f
dangkhoa <dangkhoa1699@gmail.com>
2025-03-07 18:19:44 +0700

    update Migrations

--- Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671cfea5bf97312143a8ce4605097fe1fdab0a64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73e12253aa844429d8e61e77824bc21a2955731")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
