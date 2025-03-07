4dba29d1ad7b7e347570af732ad60e2475811b04
AnhKhoaCoding <nguyenanhkhoa1699@gmail.com>
2025-03-07 18:40:34 +0700

    Add api product

--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671cfea5bf97312143a8ce4605097fe1fdab0a64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73e12253aa844429d8e61e77824bc21a2955731")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
