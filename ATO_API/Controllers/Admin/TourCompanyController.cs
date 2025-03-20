b34c002fb093ba40b267c222e99f0d2427c7fd83
AnhKhoaCoding <nguyenanhkhoa1699@gmail.com>
2025-03-20 17:21:49 +0700

    Push api for guest

--- ATO_API/Controllers/Admin/TourCompanyController.cs
+++ ATO_API/Controllers/Admin/TourCompanyController.cs
@@ -10,36 +10,32 @@ using Service.TourCompanySer;
 using Service.TouristFacilitySer;
 using Service.UserSupportSer;
 
-namespace ATO_API.Controllers.Admin
+namespace ATO_API.Controllers
 {
-    [Route("api/admin/tour-company")]
-    [Authorize(Roles = "Admin")]
+    [Route("api/tour-company")]
     [ApiController]
     public class TourCompanyController : ControllerBase
     {
         private readonly ITourCompanyService _tourCompanyService;
-        private readonly IAccountService _accountService;
         private readonly IMapper _mapper;
 
         public TourCompanyController(
             ITourCompanyService tourCompanyService,
-            IAccountService accountService,
             IMapper mapper
             )
         {
             _tourCompanyService = tourCompanyService;
-            _accountService = accountService;
             _mapper = mapper;
         }
         [HttpGet("list-tour-companies")]
-        [ProducesResponseType(typeof(List<TourCompanyDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<TourCompanyDTO_Guest>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> ListTourCompany()
         {
             try
             {
                 List<Data.Models.TourCompany> response = await _tourCompanyService.GetListTourCompanies_Admin();
-                List<TourCompanyDTO> responseResult = _mapper.Map<List<TourCompanyDTO>>(response);
+                List<TourCompanyDTO_Guest> responseResult = _mapper.Map<List<TourCompanyDTO_Guest>>(response);
                 return Ok(responseResult);
             }
             catch (Exception ex)
@@ -52,14 +48,14 @@ namespace ATO_API.Controllers.Admin
             }
         }
         [HttpGet("get-tour-company/{TourCompanyId}")]
-        [ProducesResponseType(typeof(TourCompanyDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(TourCompanyDTO_Guest), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetTourCompany(Guid TourCompanyId)
         {
             try
             {
                 Data.Models.TourCompany response = await _tourCompanyService.GetTourCompany_Admin(TourCompanyId);
-                TourCompanyDTO responseResult = _mapper.Map<TourCompanyDTO>(response);
+                TourCompanyDTO_Guest responseResult = _mapper.Map<TourCompanyDTO_Guest>(response);
                 return Ok(responseResult);
             }
             catch (Exception ex)
@@ -68,132 +64,6 @@ namespace ATO_API.Controllers.Admin
                 {
                     Status = false,
                     Message = ex.Message,
-                });
-            }
-        }
-        [HttpPost("create-tour-company")]
-        [ProducesResponseType(typeof(TourCompanyDTO), StatusCodes.Status201Created)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> CreateTourCompany([FromBody] CreateTourCompanyRequest request)
-        {
-            try
-            {
-                if (string.IsNullOrWhiteSpace(request.CompanynName))
-                {
-                    return BadRequest(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Tên công ty không được để trống."
-                    });
-                }
-                if (request.UserId == Guid.Empty)
-                {
-                    return BadRequest(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Quản lý công ty không được để trống!"
-                    });
-                }
-
-                var newTourCompany = _mapper.Map<Data.Models.TourCompany>(request);
-                newTourCompany.TourCompanyId = Guid.NewGuid();
-                newTourCompany.CreateDate = DateTime.UtcNow;
-
-                await _tourCompanyService.AddTourCompanyAsync(newTourCompany);
-
-                var response = _mapper.Map<TourCompanyDTO>(newTourCompany);
-                return CreatedAtAction(nameof(GetTourCompany), new { TourCompanyId = newTourCompany.TourCompanyId }, response);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ResponseVM
-                {
-                    Status = false,
-                    Message = ex.Message,
-                });
-            }
-        }
-        [HttpPut("update-tour-company")]
-        [ProducesResponseType(typeof(TourCompanyDTO), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> UpdateTourCompany([FromBody] UpdateTourCompanyRequest request)
-        {
-            try
-            {
-                if (request.TourCompanyId == Guid.Empty)
-                {
-                    return BadRequest(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Tour Company Id không hợp lệ."
-                    });
-                }
-
-                if (string.IsNullOrWhiteSpace(request.CompanynName))
-                {
-                    return BadRequest(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Tên công ty không được để trống."
-                    });
-                }
-
-                if (request.UserId == Guid.Empty)
-                {
-                    return BadRequest(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Quản lý công ty không được để trống!"
-                    });
-                }
-
-                var existingTourCompany = await _tourCompanyService.GetTourCompany_Admin(request.TourCompanyId);
-                if (existingTourCompany == null)
-                {
-                    return NotFound(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Không tìm thấy Tour Company."
-                    });
-                }
-
-                _mapper.Map(request, existingTourCompany);
-                existingTourCompany.UpdateTime = DateTime.UtcNow;
-
-                await _tourCompanyService.UpdateTourCompanyAsync(existingTourCompany);
-                var response = _mapper.Map<TourCompanyDTO>(existingTourCompany);
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ResponseVM
-                {
-                    Status = false,
-                    Message = ex.Message
-                });
-            }
-        }
-        [HttpGet("unassigned-tour-companies")]
-        [ProducesResponseType(typeof(IEnumerable<UserUnassignedTourCompany>), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetUnassignedTourCompanies()
-        {
-            try
-            {
-                var tourCompanies = await _accountService.GetUnassignedTourCompaniesAsync();
-
-                var response = _mapper.Map<IEnumerable<UserUnassignedTourCompany>>(tourCompanies);
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ResponseVM
-                {
-                    Status = false,
-                    Message = ex.Message
                 });
             }
         }
--- ATO_API/Controllers/Admin/TouristFacilityController.cs
+++ ATO_API/Controllers/Admin/TouristFacilityController.cs
@@ -9,35 +9,31 @@ using Service.AccountSer;
 using Service.TouristFacilitySer;
 using Service.UserSupportSer;
 
-namespace ATO_API.Controllers.Admin
+namespace ATO_API.Controllers
 {
-    [Route("api/admin/tourist-facility")]
-    [Authorize(Roles = "Admin")]
+    [Route("api/tourist-facility")]
     [ApiController]
     public class TouristFacilityController : ControllerBase
     {
         private readonly ITouristFacilityService _touristFacilityService;
-        private readonly IAccountService _accountService;
         private readonly IMapper _mapper;
         public TouristFacilityController(
             ITouristFacilityService touristFacilityService,
-            IAccountService accountService,
             IMapper mapper
             )
         {
             _touristFacilityService = touristFacilityService;
-            _accountService = accountService;
             _mapper = mapper;
         }
         [HttpGet("list-tourist-facilities")]
-        [ProducesResponseType(typeof(List<TouristFacilityDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<TouristFacilityDTO_Guest>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> ListTouristFacility()
         {
             try
             {
                 List<TouristFacility> response = await _touristFacilityService.GetListTouristFacilities_Admin();
-                List<TouristFacilityDTO> responseResult = _mapper.Map<List<TouristFacilityDTO>>(response);
+                List<TouristFacilityDTO_Guest> responseResult = _mapper.Map<List<TouristFacilityDTO_Guest>>(response);
                 return Ok(responseResult);
             }
             catch (Exception ex)
@@ -50,14 +46,14 @@ namespace ATO_API.Controllers.Admin
             }
         }
         [HttpGet("get-tourist-facility/{TouristFacilityId}")]
-        [ProducesResponseType(typeof(TouristFacilityDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(TouristFacilityDTO_Guest), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetTouristFacility(Guid TouristFacilityId)
         {
             try
             {
-                TouristFacility response = await _touristFacilityService.GetTouristFacilities_Admin(TouristFacilityId);
-                TouristFacilityDTO responseResult = _mapper.Map<TouristFacilityDTO>(response);
+                TouristFacility response = await _touristFacilityService.GetTouristFacilities_Guest(TouristFacilityId);
+                TouristFacilityDTO_Guest responseResult = _mapper.Map<TouristFacilityDTO_Guest>(response);
                 return Ok(responseResult);
             }
             catch (Exception ex)
@@ -66,132 +62,6 @@ namespace ATO_API.Controllers.Admin
                 {
                     Status = false,
                     Message = ex.Message,
-                });
-            }
-        }
-        [HttpPost("create-tourist-facility")]
-        [ProducesResponseType(typeof(TouristFacilityDTO), StatusCodes.Status201Created)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> CreateTouristFacility([FromBody] CreateTouristFacilityRequest request)
-        {
-            try
-            {
-                if (string.IsNullOrWhiteSpace(request.TouristFacilityName))
-                {
-                    return BadRequest(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Tên đơn vị cung cấp không được để trống."
-                    });
-                }
-                if (request.UserId == Guid.Empty)
-                {
-                    return BadRequest(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Quản lý đơn vị cung cấp không được để trống!"
-                    });
-                }
-
-                var newTourCompany = _mapper.Map<TouristFacility>(request);
-                newTourCompany.TouristFacilityId = Guid.NewGuid();
-                newTourCompany.CreateDate = DateTime.UtcNow;
-
-                await _touristFacilityService.AddTouristFacilitiesAsync(newTourCompany);
-
-                var response = _mapper.Map<TouristFacilityDTO>(newTourCompany);
-                return CreatedAtAction(nameof(GetTouristFacility), new { TouristFacilityId = newTourCompany.TouristFacilityId }, response);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ResponseVM
-                {
-                    Status = false,
-                    Message = ex.Message,
-                });
-            }
-        }
-        [HttpPut("update-tourist-facility")]
-        [ProducesResponseType(typeof(TouristFacilityDTO), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status404NotFound)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> UpdateTouristFacility([FromBody] UpdateTouristFacilityRequest request)
-        {
-            try
-            {
-                if (request.TouristFacilityId == Guid.Empty)
-                {
-                    return BadRequest(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Id không hợp lệ."
-                    });
-                }
-
-                if (string.IsNullOrWhiteSpace(request.TouristFacilityName))
-                {
-                    return BadRequest(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Tên đơn vị cung cấp không được để trống."
-                    });
-                }
-
-                if (request.UserId == Guid.Empty)
-                {
-                    return BadRequest(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Quản lý đơn vị cung cấp không được để trống!"
-                    });
-                }
-
-                var existingTouristFacility = await _touristFacilityService.GetTouristFacilities_Admin(request.TouristFacilityId);
-                if (existingTouristFacility == null)
-                {
-                    return NotFound(new ResponseVM
-                    {
-                        Status = false,
-                        Message = "Không tìm thấy đơn vị cung cấp."
-                    });
-                }
-
-                _mapper.Map(request, existingTouristFacility);
-                existingTouristFacility.UpdateTime = DateTime.UtcNow;
-
-                await _touristFacilityService.UpdateTouristFacilitiesAsync(existingTouristFacility);
-                var response = _mapper.Map<TouristFacilityDTO>(existingTouristFacility);
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ResponseVM
-                {
-                    Status = false,
-                    Message = ex.Message
-                });
-            }
-        }
-        [HttpGet("unassigned-tour-facility")]
-        [ProducesResponseType(typeof(IEnumerable<UserUnassignedTourCompany>), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ResponseVM), StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetUnassignedTouristFacilities()
-        {
-            try
-            {
-                var tourCompanies = await _accountService.GetUnassignedTouristFacilitiesAsync();
-
-                var response = _mapper.Map<IEnumerable<UserUnassignedTourCompany>>(tourCompanies);
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ResponseVM
-                {
-                    Status = false,
-                    Message = ex.Message
                 });
             }
         }
