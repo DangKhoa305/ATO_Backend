4a8b9123ceccdccb386d6ae4858461d6b5311c5e
dangkhoa <dangkhoa1699@gmail.com>
2025-04-25 00:49:27 +0700

    .

---
+++ Service/ContractSer/IContractService.cs
@@ -0,0 +1,23 @@
+﻿using Data.Models;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.ContractSer
+{
+    public interface IContractService
+    {
+        Task<List<Contract>> ListContract();
+        Task<Contract> GetContract(Guid ContractId);
+        Task<bool> AddContract(Contract Contract);
+        Task<bool> UpdateContract(Guid ContractId, Contract Contract);
+        Task<List<Contract>> ListContractTourCompany(Guid userId);
+        Task<bool> SendOTPAsync(Guid contractId);
+        Task<bool> SignContractAsync(Guid contractId);
+        Task<List<Data.Models.Contract>> ListContractFacility(Guid userId);
+    }
+}
